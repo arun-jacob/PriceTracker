e0f5da1580da622760e18ce5110ee683cd416d49
Arun Jacob <arunzachariahjacob@gmail.com>
2019-08-25 12:19:55 +0530

    fix *02

--- Scrapper.py
+++ Scrapper.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 
 URL='https://www.amazon.in/BLACK-DECKER-CD121K50-Cordless-Accessories/dp/B00R4R1DL2/ref=sr_1_10?keywords=drill&qid=1566713966&s=gateway&sr=8-10#customerReviews'
 
-header={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36" }
+headers={"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36" }
 
-page=requests.get(URL,header=header)
+page=requests.get(URL,headers=headers)
 
 soup=BeautifulSoup(page.content,'html.parser')
 print(soup.prettify())
